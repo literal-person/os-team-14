4565408232e3d7fab72e5ee9b84d9808f9f40635
Mark Mannion <gmark.mannion@gmail.com>
2026-03-01 11:11:48 +0000

    Finished read_gamepad() & fixed prototypes + declarations

--- lkm_char_driver/lkm.c
+++ lkm_char_driver/lkm.c
@@ -1,16 +1,18 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
-#include <linux/uaccess.h>
+#include <linux/uaccess.h> //for copy_to/from_user
 #include <linux/ioctl.h>
-#include <linux/wait.h>
+#include <linux/wait.h> //DECLARE_WAIT_QUEUE_HEAD & wait_event_interruptible
 #include <linux/sched.h>
+#include <linux/proc_fs.h> //for proc files
 
 
 //name of device, variable for major number,cdev structure
 #define DEVICE_NAME "gamepad"
 static int major;
 static struct cdev cdev;
+static struct class *gamepad_class;
 
 //need-> struct to map the gamepad buttons
 
@@ -21,12 +23,14 @@ static struct cdev cdev;
 
 //need -> read write variables
 static int button_pressed = 0;
+static char button_id = 0;
+static DECLARE_WAIT_QUEUE_HEAD(read_wait);
 
 
 //func prototypes
-static int gamepad_open(Struct inode *, struct file *);
-static int gamepad_release(struct inode *, struct file *);
-static ssize_t read_gamepad(struct file *, const char __user *, size_t, loff_t *);
+static int open_gamepad(struct inode *, struct file *);
+static int release_gamepad(struct inode *, struct file *);
+static ssize_t read_gamepad(struct file *, char __user *, size_t, loff_t *);
 static long ioctl_gamepad(struct file *, unsigned int, unsigned long);
 
 //don't think we need this for our thing
@@ -44,29 +48,36 @@ static const struct file_operations gamepad_fops = {
   //will prolly need 1 or 2 more here
 };
 
-static int gamepad _open(struct inode *inode, struct file *file){
+static int open_gamepad(struct inode *inode, struct file *file){
   pr_info("lkm - Gamepad device opened\n");
   return 0;
 }
 
-static int gamepad_release(struct inode *inode, struct file *file){
-  pr_info("lkm - Gamepad device closed");
+static int release_gamepad(struct inode *inode, struct file *file){
+  pr_info("lkm - Gamepad device closed\n");
   return 0;
 }
 
 
 //reading and blocking until it dectects a button press
 static ssize_t read_gamepad(struct file *file, char __user *buf, size_t count, loff_t *fpos){
-  wait_event_interruptible(read_wait, pressed_button != 0);
+  wait_event_interruptible(read_wait, button_pressed != 0);
+
+  if(copy_to_user(buf, &button_id, 1)!=0){
+    return -EFAULT;
+  }
+
+  button_pressed = 0;
+  return 1;
 }
 
 //just return an error if it tries to write to our gamepad because it shouldn't be
-static ssize_t write_gamepad(struct file *, const char __user *, size_t, loff_t *);{
+static ssize_t write_gamepad(struct file *file, const char __user *buf, size_t count, loff_t *f_pos){
   return -EINVAL;
 }
 
 //func for calling ioctl commands
-static long gamepad_ioctl(struct file *file, unsigned int cmd, unsigned long arg){
+static long ioctl_gamepad(struct file *file, unsigned int cmd, unsigned long arg){
 
 }
 
