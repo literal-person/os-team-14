edee75fd5978724376c20f7841bb1a53bb69cfb3
Mark Mannion <gmark.mannion@gmail.com>
2026-03-01 23:07:04 +0000

    Finished the __init function

--- lkm_char_driver/lkm.c
+++ lkm_char_driver/lkm.c
@@ -5,10 +5,10 @@
 #include <linux/ioctl.h>
 #include <linux/wait.h> //DECLARE_WAIT_QUEUE_HEAD & wait_event_interruptible
 #include <linux/sched.h>
-#include <linux/proc_fs.h> //for proc files
+#include <linux/proc_fs.h>
 
 
-//name of device, variable for major number,cdev structure
+//name of device, variable for major number,cdev structure, device class
 #define DEVICE_NAME "gamepad"
 static int major;
 static struct cdev cdev;
@@ -19,22 +19,17 @@ struct button_mapper{
   char button_id;
   char command[256];
 }
-//need-> to define the ioctl command for mapping buttons
+//NEED-> to define the ioctl command for mapping buttons
 
-
-//need -> read write variables
 static int button_pressed = 0;
 static char button_id = 0;
 static DECLARE_WAIT_QUEUE_HEAD(read_wait);
-
 
 //func prototypes
 static int open_gamepad(struct inode *, struct file *);
 static int release_gamepad(struct inode *, struct file *);
 static ssize_t read_gamepad(struct file *, char __user *, size_t, loff_t *);
 static long ioctl_gamepad(struct file *, unsigned int, unsigned long);
-
-//don't think we need this for our thing
 static ssize_t write_gamepad(struct file *, const char __user *, size_t, loff_t *);
 
 
@@ -46,7 +41,6 @@ static const struct file_operations gamepad_fops = {
   .read = read_gamepad,
   .write = write_gamepad,
   .unlocked_ioctl = ioctl_gamepad,
-  //will prolly need 1 or 2 more here
 };
 
 static int open_gamepad(struct inode *inode, struct file *file){
@@ -60,7 +54,7 @@ static int release_gamepad(struct inode *inode, struct file *file){
 }
 
 
-//done -> reading and blocking until it dectects a button press
+//Mark: DONE -> reading and blocking until it dectects a button press
 static ssize_t read_gamepad(struct file *file, char __user *buf, size_t count, loff_t *fpos){
   wait_event_interruptible(read_wait, button_pressed != 0);
 
@@ -77,30 +71,50 @@ static ssize_t write_gamepad(struct file *file, const char __user *buf, size_t c
   return -EINVAL;
 }
 
-//Cameron: need -> func for calling ioctl commands
+//Cameron: NEED -> func for calling ioctl commands
 static long ioctl_gamepad(struct file *file, unsigned int cmd, unsigned long arg){
 
 }
 
-//Mark: need -> func gamepad_init for when module is initially loaded
+//Mark: DONE -> func gamepad_init for when module is initially loaded
 static int __init gamepad_init(void){
+    //allocating its device number
+  if(alloc_chrdev_region(&major, 0, 1, DEVICE_NAME)<0){
+    pr_alert("Failed to allocate device number\n");
+    return -1;
+  }
 
+  //making its device class for /dev/
+  gamepad_class = class_create(THIS_MODULE, DEVICE_NAME);
+  if(IS_ERR(gamepad_class)) {
+    unregister_chrdev_region(major, 1);
+    return PTR_ERR(gamepad_class);
+  }
+  //making its device file
+  device_creat(gamepad_class, NULL, MKDEV(major, 0), NULL, DEVICE_NAME)
+
+  //for creating the character device
+  cdev_init(&cdev, &gamepad_fops);
+  cdev_add(&cdev, MKDEV(major, 0), 1);
+
+  //make its procfile in /proc/
+  proc_create("stats_gamepad", 0, NULL, &stats_proc_ops); //need to make the proc fops struct for this to work
+  pr_info("Initialised your Gamepad. Major number is: %d\n", major);
+  return 0;
 }
 
-//Cameron: need -> func gamepad_exit for when removing module when finished
+//Cameron: NEED -> func gamepad_exit for when removing module when finished
 static void __exit gamepad_exit(void){
 
 }
 
 
-//also need some func to create a proc file with info about the device/lkm
+//NEED -> some funcs to create a proc file with info about the device/lkm
 
 //start and finish the lkm
 module_init(gamepad_init);
 module_exit(gamepad_exit);
-
-
 //just general module info you can call in the terminal
-MODULE_LICENSE("MIT");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Mark, Cameron);
 MODULE_DESCRIPTION("A gamepad Character device driver");
