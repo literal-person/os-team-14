088b5be25cdc662e21a80ab8f92e2fb39eb5caef
literal-person <alancai6969@gmail.com>
2026-03-01 22:10:54 +0000

    feat: added comments to app

---
+++ userspace_app/gamepad_app.c
@@ -0,0 +1,5 @@
+//getting data from copy_to_user
+//we receive the button id
+//parse id/possibly against hashmap where the k/v pair is id to command/mouse movement
+//this button id is stored in proc/read_gamepad
+//if the id is an error, print error message
